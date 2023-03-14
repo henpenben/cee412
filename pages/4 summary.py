e0a01caa6174ece7dcfe42844c514917272045db
henpenben <henryhow@uw.edu>
2023-03-13 21:02:15 -0700

    f

---
+++ pages/4 summary.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+'''# Summary'''
+
+'''
+there are a lot of accidents
+
+since we don't have vehicle miles traveled we cannot say for certain if younger drivers crash more frequently
+
+even still, the quantity of accidents in just the range of 16-21 is nearly 500, and the 33 year range of 31-65 has 1806. If the 16-21 age groups had the same number of years, it would be about 3000 crashes.
+'''
